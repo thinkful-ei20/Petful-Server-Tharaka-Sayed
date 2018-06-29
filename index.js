c3cb51dd6af666ec2d7a75dbe57f661aa23f4faa
Tharaka Wijekularatne <tharusnow@gmail.com>
2018-06-29 08:48:21 -0700

    added get and delete

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 'use strict';
 
+let cats = [{
+  imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
+}];
+
+let dogs = [{
+  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+  imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+  name: 'Zeus',
+  sex: 'Male',
+  age: 3,
+  breed: 'Golden Retriever',
+  story: 'Owner Passed away'
+}];
+
+const catLoop = function(arr){
+  for(let i = 0; i < arr.length; i++) {
+    return arr[i];
+  }
+}
+
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
@@ -7,6 +33,7 @@ const morgan = require('morgan');
 const { PORT, CLIENT_ORIGIN } = require('./config');
 const { dbConnect } = require('./db-mongoose');
 // const {dbConnect} = require('./db-knex');
+
 
 const app = express();
 
@@ -21,6 +48,26 @@ app.use(
     origin: CLIENT_ORIGIN
   })
 );
+
+//Get Cats
+app.get('/api/cat', (req, res) => {
+  res.json(cats[0]);
+});
+
+//Get Dogs
+app.get('/api/dog', (req, res) => {
+  res.json(dogs[0]);
+});
+
+//Delete Cats
+app.delete('/api/cat', (req, res) => {
+  res.json(cats.shift());
+})
+
+//Delete Dogs
+app.delete('/api/dog', (req, res) => {
+  res.json(dogs.shift());
+})
 
 function runServer(port = PORT) {
   const server = app
