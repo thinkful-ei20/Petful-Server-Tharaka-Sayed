7134a705e2b5eab76521d64d740c20c762cd1d8e
arsalon <arsalon47@gmail.com>
2018-06-29 15:14:48 -0400

    working on queue

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
 'use strict';
 
-// let cats = [{
-//   imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
-//   imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
-//   name: 'Fluffy',
-//   sex: 'Female',
-//   age: 2,
-//   breed: 'Bengal',
-//   story: 'Thrown on the street'
-// }];
-
-// let dogs = [{
-//   imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
-//   imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
-//   name: 'Zeus',
-//   sex: 'Male',
-//   age: 3,
-//   breed: 'Golden Retriever',
-//   story: 'Owner Passed away'
-// }];
-
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
@@ -27,7 +7,7 @@ const morgan = require('morgan');
 const { PORT, CLIENT_ORIGIN } = require('./config');
 // const { dbConnect } = require('./db-mongoose');
 // const {dbConnect} = require('./db-knex');
-const { peek, Queue, _Node, enqueue, dequeue } = require('./queue');
+const { peek, Queue } = require('./queue');
 
 
 const app = express();
@@ -49,18 +29,30 @@ const dogQueue = new Queue();
 
 
 catQueue.enqueue(
-  {  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  }
+);
+
+dogQueue.enqueue({
+  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
   imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
   name: 'Zeus',
   sex: 'Male',
   age: 3,
   breed: 'Golden Retriever',
-  story: 'Owner Passed away'}
-)
+  story: 'Owner Passed away'
+});
 
 //Get Cats
 app.get('/api/cat', (req, res) => {
-  res.json(cats[0]);
+  res.json(peek(catQueue));
 });
 
 //Get Dogs
@@ -71,12 +63,12 @@ app.get('/api/dog', (req, res) => {
 
 //Delete Cats
 app.delete('/api/cat', (req, res) => {
-  res.json(cats.shift());
+  catQueue.dequeue();
 })
 
 //Delete Dogs
 app.delete('/api/dog', (req, res) => {
-  res.json(dogs.shift());
+  dogQueue.dequeue();
 })
 
 function runServer(port = PORT) {
